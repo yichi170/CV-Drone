d6a047424ff1a56584776cb7f24a7d91c9351a0c
yichi170 <ilee900620@gmail.com>
2022-03-17 22:07:48 +0800

    4-2

--- lab04/lab4-2.py
+++ lab04/lab4-2.py
@@ -1,18 +1,37 @@
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(1)
+frame = cv2.imread('broadway.jpg')
+height, width = frame.shape[:2]
+print(height, width)
+# cv2.imshow('frame', frame)
+# cv2.waitKey(0)
+# cv2.getPerspectiveTransform()
+
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
+    ret, cam = cap.read()
     if ret == False:
         break
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    camheight, camwidth = cam.shape[:2]
+    
+    out_frame = np.copy(frame)
+    
+    src = np.float32([[0, 0], [camwidth -1, 0], [camwidth - 1,camheight - 1], [0, camheight - 1]])
+    dst = np.float32([[202, 422], [426, 423], [421, 631], [99, 611]])
+    dst = np.float32([[422, 202], [426, 423], [631, 421], [611, 101]])
+    # dst = src/2 +200
+    pmat = cv2.getPerspectiveTransform(src, dst)
+    # print(pmat)
+    # warped = cv2.warpPerspective(cam, pmat, (width, height))
 
-    ret, corners = cv2.findChessboardCorners(gray, )
-
-    if ret == True:
-        
-
-    cv2.imshow('frame', frame)
+    for i in range(cam.shape[0]):
+        for j in range(cam.shape[1]):
+            trans = np.dot(pmat, np.array([i, j, 1]))/np.dot(pmat, np.array([i, j, 1]))[2]
+            # trans = np.matmul(pmat, np.array([i, j, 1]))
+            out_frame[trans[0].astype(int),trans[1].astype(int)] = cam[i,j]
+    cv2.imshow('frame2', out_frame)
     cv2.waitKey(33)
+    # print(warped.shape)
