5ba4657fb37a7bd2986b91ae233adc34e09448eb
LAPTOP-OO65L1US\user <notspecialman@gmail.com>
2022-03-23 16:35:54 +0800

    wu

--- lab04/lab4-2.py
+++ lab04/lab4-2.py
@@ -19,9 +19,10 @@ while True:
     
     out_frame = np.copy(frame)
     
-    src = np.float32([[0, 0], [camwidth -1, 0], [camwidth - 1,camheight - 1], [0, camheight - 1]])
-    dst = np.float32([[202, 422], [426, 423], [421, 631], [99, 611]])
-    dst = np.float32([[422, 202], [426, 423], [631, 421], [611, 101]])
+    # src = np.float32([[0, 0], [camwidth -1, 0], [camwidth - 1,camheight - 1], [0, camheight - 1]])
+    src = np.float32([[0, 0], [camheight -1, 0], [camheight - 1,camwidth - 1], [0, camwidth - 1]])
+    dst = np.float32([[205, 420], [425, 425], [420, 636], [100, 610]])
+    # dst = np.float32([[422, 202], [426, 423], [631, 421], [611, 101]])
     # dst = src/2 +200
     pmat = cv2.getPerspectiveTransform(src, dst)
     # print(pmat)
@@ -29,9 +30,13 @@ while True:
 
     for i in range(cam.shape[0]):
         for j in range(cam.shape[1]):
-            trans = np.dot(pmat, np.array([i, j, 1]))/np.dot(pmat, np.array([i, j, 1]))[2]
+            # trans = np.dot(pmat, np.array([i, j, 1]))/np.dot(pmat, np.array([i, j, 1]))[2]
+            trans = np.dot(pmat, np.array([i, j, 1]))
+            trans = trans/trans[2]
             # trans = np.matmul(pmat, np.array([i, j, 1]))
             out_frame[trans[0].astype(int),trans[1].astype(int)] = cam[i,j]
     cv2.imshow('frame2', out_frame)
-    cv2.waitKey(33)
+    k = cv2.waitKey(33)
+    if k == 27:
+        break
     # print(warped.shape)
