c3b11e1e4b3c458254329c0026af649cd19a88c7
yichi170 <ilee900620@gmail.com>
2022-03-31 21:47:04 +0800

    lab6 not yet

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -3,7 +3,7 @@ from djitellopy import Tello
 import cv2
 import numpy as np
 import time
-# from tello_control_ui import TelloUI
+import math
 from pyimagesearch.pid import PID
 
 filename = "out.xml"
@@ -11,8 +11,9 @@ textColor = (0, 225, 225)
 textthickness = 2
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
+max_speed_threshold = 40
 
-def keyboard(self, key):
+def keyboard(drone, key):
     #global is_flying
     print("key:", key)
     fb_speed = 40
@@ -20,51 +21,54 @@ def keyboard(self, key):
     ud_speed = 50
     degree = 30
     if key == ord('1'):
-        self.takeoff()
+        drone.takeoff()
         #is_flying = True
     if key == ord('2'):
-        self.land()
+        drone.land()
         #is_flying = False
     if key == ord('3'):
-        self.send_rc_control(0, 0, 0, 0)
+        drone.send_rc_control(0, 0, 0, 0)
         print("stop!!!!")
     if key == ord('w'):
-        self.send_rc_control(0, fb_speed, 0, 0)
+        drone.send_rc_control(0, fb_speed, 0, 0)
         print("forward!!!!")
     if key == ord('s'):
-        self.send_rc_control(0, (-1) * fb_speed, 0, 0)
+        drone.send_rc_control(0, (-1) * fb_speed, 0, 0)
         print("backward!!!!")
     if key == ord('a'):
-        self.send_rc_control((-1) * lf_speed, 0, 0, 0)
+        drone.send_rc_control((-1) * lf_speed, 0, 0, 0)
         print("left!!!!")
     if key == ord('d'):
-        self.send_rc_control(lf_speed, 0, 0, 0)
+        drone.send_rc_control(lf_speed, 0, 0, 0)
         print("right!!!!")
     if key == ord('z'):
-        self.send_rc_control(0, 0, ud_speed, 0)
+        drone.send_rc_control(0, 0, ud_speed, 0)
         print("down!!!!")
     if key == ord('x'):
-        self.send_rc_control(0, 0, (-1) *ud_speed, 0)
+        drone.send_rc_control(0, 0, (-1) *ud_speed, 0)
         print("up!!!!")
     if key == ord('c'):
-        self.send_rc_control(0, 0, 0, degree)
+        drone.send_rc_control(0, 0, 0, degree)
         print("rotate!!!!")
     if key == ord('v'):
-        self.send_rc_control(0, 0, 0, (-1) *degree)
+        drone.send_rc_control(0, 0, 0, (-1) *degree)
         print("counter rotate!!!!")
     if key == ord('5'):
-        height = self.get_height()
+        height = drone.get_height()
         print(height)
     if key == ord('6'):
-        battery = self.get_battery()
+        battery = drone.get_battery()
         print (battery)
+
+def control_drone(tvec):
+    print("z-update: ", z_update)
 
 def main():
     
     drone = Tello()
     drone.connect()
 
-    time.sleep(10)
+    time.sleep(13)
 
     print("breakpoint1")
     
@@ -85,8 +89,9 @@ def main():
     print("breakpoint3")
 
     while True:
+        drone.streamon()
         frame = drone.get_frame_read()
-        print("breakpoint4")
+        # print("breakpoint4")
         frame = frame.frame
         frame2 = frame.copy()
 
@@ -95,21 +100,71 @@ def main():
         markerConers, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(frame2, dictionary, parameters=parameters)
         frame2 = cv2.aruco.drawDetectedMarkers(frame2, markerConers, markerIds)
         rvec, tvec, _objPoints = cv2.aruco.estimatePoseSingleMarkers(markerConers, 15, intrinsic, distortion)
-        print(rvec)
-        print(tvec)
-        print(_objPoints)
+        # print(rvec)
+        # print(tvec)
+        # print(_objPoints)
+
+        key = cv2.waitKey(33)
+
+        if key != -1:
+            keyboard(drone, key)
+
         if rvec is not None and tvec is not None:
-            frame2 = cv2.aruco.drawAxis(frame2, intrinsic, distortion, rvec, tvec, 5)
-            frame2 = cv2.putText(frame2, 
-                    f'x: {tvec[0][0][0]:.2f} y: {tvec[0][0][1]:.2f} z: {tvec[0][0][2]:.2f}',
-                    (00, 185), font, fontScale, textColor, textthickness, cv2.LINE_AA, False)
+            # frame2 = cv2.aruco.drawAxis(frame2, intrinsic, distortion, rvec, tvec, 5)
+            # frame2 = cv2.putText(frame2, 
+            #         f'x: {tvec[0][0][0]:.2f} y: {tvec[0][0][1]:.2f} z: {tvec[0][0][2]:.2f}',
+            #         (00, 185), font, fontScale, textColor, textthickness, cv2.LINE_AA, False)
+            
+            # control_drone(drone, tvec)
+
+            z_update = tvec[0, 0, 2] - 100
+            print("org_z: " + str(z_update))
+            z_update = z_pid.update(z_update, sleep=0)
+            print("pid_z: " + str(z_update))
+            if z_update > max_speed_threshold:
+                z_update = max_speed_threshold
+            elif z_update < -max_speed_threshold:
+                z_update = -max_speed_threshold
+            # drone.send_rc_control(0, int(z_update // 2), 0, 0)
+
+            y_update = tvec[0, 0, 1] - 8
+            print("org_y: " + str(y_update))
+            y_update = y_pid.update(y_update, sleep=0) - 8
+            print("pid_y: " + str(y_update))
+            if y_update > max_speed_threshold:
+                y_update = max_speed_threshold
+            elif y_update < -max_speed_threshold:
+                y_update = -max_speed_threshold
+
+            # drone.send_rc_control(0, int(z_update // 2), 0, 0)
+            drone.send_rc_control(0, 0, int(-y_update // 2), 0)
+
+
+            dst, jaco = cv2.Rodrigues(rvec)
+            z_ = np.array([dst[0][2], dst[1][2], dst[2][2]])
+            v = np.array([z_[0], 0, z_[2]])
+            degree = math.atan2(z_[2], z_[0])
+            degree = -degree * 180 / math.pi
+            print("ori_degree: ", degree)
+            degree -= 85
+            if degree > 30:
+                degree = 30
+            elif degree < -30:
+                degree = -30
+            print("after degree: ", degree)
+            drone.send_rc_control(0, 0, 0, int(degree))
+            print("degree: ", degree)
+            if int(degree) > 0:
+                print("right")
+            elif int(degree) < 0:
+                print("left")
 
 
         cv2.imshow("drone", frame2) 
         key = cv2.waitKey(33)
 
         if key != -1:
-            drone.keyboard(key)
+            keyboard(drone, key)
 
     cv2.destroyAllWindows()
 
